f8c8b1980c5ddf5ac525714e71916e3bb4868f8b
DiegoRivas1 <rhyi888@gmail.com>
2025-10-04 20:08:14 -0500

    Actualización de URLs para despliegue correcto en stiffness-matrix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/MATRIZ_RIGIDEZ/",
+  base: "/stiffness-matrix/",
 })
